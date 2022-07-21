eee2fda5a2b85fefd8eb9de9981bf907d4d29e8f
jbartlett <jen10bart@gmail.com>
2022-07-21 09:27:18 -0400

    blog post

--- website/models.py
+++ website/models.py
@@ -13,10 +13,5 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
-    text1 = db.Column(db.Text, nullable=False)
-    text2 = db.Column(db.Text, nullable=False)
-    text3 = db.Column(db.Text, nullable=False)
-    text4 = db.Column(db.Text, nullable=False)
-    text5 = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
