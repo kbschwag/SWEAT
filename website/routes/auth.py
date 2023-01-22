50fe4e93d64cefb7cb33e5dd92265f8224696635
George Parks <60108810+geoge23@users.noreply.github.com>
2023-01-22 18:10:39 -0500

    moved db to models for better concern separation

--- website/routes/auth.py
+++ website/routes/auth.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from app import db
-from models import User
+from models import db, User
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_wtf import RecaptchaField
 from flask import *
--- website/routes/views.py
+++ website/routes/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from models import Post, User, Comment, Like, Browsers, Consents
-from app import db
+from models import Post, User, Comment, Like, Browsers, Consents, db
 from markupsafe import Markup
 from jinja2.utils import markupsafe
 markupsafe.Markup()
