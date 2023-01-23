4ef78bd6bc8483579875e9110b62960f654d41c3
George Parks <60108810+geoge23@users.noreply.github.com>
2023-01-22 19:41:20 -0500

    Added reset password

--- website/routes/auth.py
+++ website/routes/auth.py
@@ -28,6 +28,58 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+@auth.route("/forgot-password", methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            token = secrets.token_urlsafe(16)
+            new_verification = Verification(user_id=user.id, intent="reset-password", token=token)
+            db.session.add(new_verification)
+
+            send_templated_email(
+                email,
+                "forgot_password.html",
+                {
+                    "subject": "Reset your password",
+                    "link": url_for('auth.reset_password', token=token)
+                }
+            )
+
+            db.session.commit()
+
+            flash('Check your email for instructions.', category='success')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("forgot_password.html", user=current_user)
+
+@auth.route("/reset-password", methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'POST':
+        token = request.args.get('token')
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        verification = Verification.query.filter_by(token=token, intent="reset-password").first()
+
+        if verification:
+            if password1 != password2:
+                flash('Passwords don\'t match!', category='error')
+            elif len(password1) < 5:
+                flash('password is too short.', category='error')
+            else:
+                user = User.query.filter_by(id=verification.user_id).first()
+                user.password = generate_password_hash(password1, method='sha256')
+                db.session.delete(verification)
+                db.session.commit()
+                flash('Password changed.', category='success')
+                return redirect(url_for('auth.login'))
+        else:
+            flash('Invalid token.', category='error')
+
+    return render_template("reset_password.html", user=current_user)
+
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
@@ -95,7 +147,7 @@ def sign_up():
 @auth.route("/verify-email", methods=['GET', 'POST'])
 def verify_email():
     token = request.args.get('token')
-    verification = Verification.query.filter_by(token=token).first()
+    verification = Verification.query.filter_by(token=token, intent="verify-email").first()
     if verification:
         user = User.query.filter_by(id=verification.user_id).first()
         user.email_verified = True
